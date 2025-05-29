c5d3a3fb0ca17783321529cfd660959c7060a810
hilmiugurpolat <hilmip13@gmail.com>
2025-05-29 22:28:09 +0300

    first commit

---
+++ AuthMicroservice/src/main/java/com/hilmiUgurPolat/AuthApplication.java
@@ -0,0 +1,12 @@
+package com.hilmiUgurPolat;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args){
+        SpringApplication.run(AuthApplication.class);
+    }
+}
