26ed34f21f4cb27bbf763dba894965285e819933
hilmiugurpolat <hilmip13@gmail.com>
2025-06-13 01:38:17 +0300

    Feign Client Entegration

--- AuthMicroservice/src/main/java/com/hilmiUgurPolat/config/RestApis.java
+++ AuthMicroservice/src/main/java/com/hilmiUgurPolat/config/RestApis.java
@@ -8,9 +8,13 @@ public class RestApis {
     public static final String VERSIONS ="/v1";
 
 
+
     public static final String AUTHSERVICE =DEVELOPER+VERSIONS+"/auth";
     public static final String REGISTER ="/register";
     public static final String LOGIN ="/login";
+
+    public static final String CREATE_USER = "/create-user";
+
 
 
 }
--- AuthMicroservice/src/main/java/com/hilmiUgurPolat/service/AuthService.java
+++ AuthMicroservice/src/main/java/com/hilmiUgurPolat/service/AuthService.java
@@ -1,8 +1,10 @@
 package com.hilmiUgurPolat.service;
 
+import com.hilmiUgurPolat.dto.CreateUserRequestDto;
 import com.hilmiUgurPolat.dto.LoginResponseDTO;
 import com.hilmiUgurPolat.dto.RegisterRequestDTO;
 import com.hilmiUgurPolat.entity.Auth;
+import com.hilmiUgurPolat.manager.UserProfileManager;
 import com.hilmiUgurPolat.repository.AuthRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,17 +14,23 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final AuthRepository authRepository;
+    private final UserProfileManager userProfileManager;
 
     public Auth register(RegisterRequestDTO dto) {
-        return authRepository.save(Auth.builder()
+        Auth auth = authRepository.save(Auth.builder()
                 .userName(dto.getUsername())
                 .email(dto.getEmail())
                 .password(dto.getPassword())
                 .build());
-
+        userProfileManager.createUser(CreateUserRequestDto.builder()
+                .authId(auth.getId())
+                .email(auth.getEmail())
+                .userName(auth.getUserName())
+                .build());
+        return auth;
     }
 
     public Boolean login(LoginResponseDTO dto) {
-        return authRepository.existsByUserNameAndPassword(dto.getUserName(),dto.getPassword());
+        return authRepository.existsByUserNameAndPassword(dto.getUserName(), dto.getPassword());
     }
 }
